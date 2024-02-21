58b052b702fa9680b3f1edb37a424519f133d14d
carloshakim00 <carloshakim00@gmail.com>
2024-02-21 17:43:39 +0700

    pair

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require("./Routers")
+const app = express()
+const port = 3000
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(router)
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
