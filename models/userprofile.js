d29a76a652b967898fc52dd9980217ebb112904a
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-02-22 20:44:33 +0700

    Updated Models

--- models/userprofile.js
+++ models/userprofile.js
@@ -15,9 +15,42 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   UserProfile.init({
-    name: DataTypes.STRING,
-    address: DataTypes.STRING,
-    phone: DataTypes.STRING,
+    name: {
+      type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Name is required.",
+          },
+          notNull: {
+            msg: "Name is required.",
+          }
+        },
+    },
+    address: {
+      type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Address is required.",
+          },
+          notNull: {
+            msg: "Address is required.",
+          }
+        },
+    },
+    phone: {
+      type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: {
+            msg: "Phone Number is required.",
+          },
+          notNull: {
+            msg: "Phone Number is required.",
+          }
+        },
+    },
     UserId: {
       type: DataTypes.INTEGER,
       references: {
