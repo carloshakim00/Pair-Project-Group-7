469dc99a4e0975f4823f30c227fc066efd74419c
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-02-22 11:14:50 +0700

    Routers

--- Routers/index.js
+++ Routers/index.js
@@ -1,25 +1,27 @@
 const express = require("express");
 const Controller = require("../Controllers/Controller");
+const LoginController = require("../Controllers/index");
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.redirect("/home");
 })
 
-router.get("/home", Controller.login);
+router.get("/home", LoginController.login);
+router.post("/home", LoginController.adduser);
 
-router.get("/home/product", Controller.showProduct);
+// router.get("/home/product", Controller.showProduct);
 
-router.get("/home/product/addProduct", Controller.addProduct);
-router.post("/home/product/addProduct", Controller.saveProduct);
+// router.get("/home/product/addProduct", Controller.addProduct);
+// router.post("/home/product/addProduct", Controller.saveProduct);
 
-router.get("/home/product/restockProduct/:productId", Controller.editProduct);
-router.post("/home/product/restockProduct/:productId", Controller.updateProduct);
+// router.get("/home/product/restockProduct/:productId", Controller.editProduct);
+// router.post("/home/product/restockProduct/:productId", Controller.updateProduct);
 
-router.get("/home/product/deleteProduct/:productId", Controller.deleteProduct);
+// router.get("/home/product/deleteProduct/:productId", Controller.deleteProduct);
 
-router.get("/home/product/buyProduct/:productId", Controller.buyProduct);
+// router.get("/home/product/buyProduct/:productId", Controller.buyProduct);
 
-router.get("/home/product/showProduct/:productId", Controller.showBuyProduct);
+// router.get("/home/product/showProduct/:productId", Controller.showBuyProduct);
 
 module.exports = router;
