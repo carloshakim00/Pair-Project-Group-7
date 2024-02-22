68a4b817714c617263d0cfad6e5a52a6a27890ef
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-02-22 11:20:08 +0700

    Login page

--- Routers/index.js
+++ Routers/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const Controller = require("../Controllers/Controller");
+const LoginController = require("../Controllers/index");
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.redirect("/home");
 })
 
-router.get("/home", Controller.login);
+router.get("/home", LoginController.login);
+router.post("/home", LoginController.addUser);
 
 // router.get("/home/product", Controller.showProduct);
 
