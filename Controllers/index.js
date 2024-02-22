9d7fe5baab62fef68eb47d887da58b1b5b51aece
carloshakim00 <carloshakim00@gmail.com>
2024-02-22 13:36:42 +0700

    validate

--- Controllers/index.js
+++ Controllers/index.js
@@ -5,7 +5,7 @@ class LoginController {
         const {error} = req.query;
         try {
             const userData = await User.findAll();
-            res.render("login", {userData, error});
+            res.render("login", {userData , error});
         } catch (error) {
             console.log(error);
             res.send(error.message);
@@ -15,20 +15,22 @@ class LoginController {
     static async addUser(req, res) {
         try {
             const {email, password, role} = req.body
-            // await User.create({email, password, role});
+            await User.create({email, password, role});
             if (role === `Seller`) {
                 res.redirect("sellerHomePage")
             } else if (role === `Buyer`) {
                 res.redirect("buyerHomePage")
-            }   
+            }
         } catch (error) {
             console.log(error);
-            if(error.name === "SequelizeValidationError") {
-                const message = error.errors.map((el) => el.message);
-                res.redirect(`/home?error=${message}`);
-            } else {
+            if (error.name == "SequelizeValidationError") {
+                let errormsg = error.errors.map((el) => {
+                  return el.message;
+                });
+                res.redirect(`/home?error=${errormsg}`);
+              } else {
                 res.send(error.message);
-            }
+              }
         }
     }
 }
