4f2b80105ede810c0c2ee07ddfdf3a0dfd6320d8
carloshakim00 <carloshakim00@gmail.com>
2024-02-22 14:49:30 +0700

    navbar

--- Controllers/Controller.js
+++ Controllers/Controller.js
@@ -5,8 +5,18 @@ class Controller{
 
     static async showSeller(req,res){
        try {
-        const products = await Product.findAll();
-        res.render('sellerView', { products});
+        let { search } = req.query
+        let option = {
+        };
+        if (search){
+            option.where = {
+                name:{
+                    [Op.iLike]: `%${search}%`,
+                },
+            };
+        }
+        const products = await Product.findAll(option);
+        res.render('sellerView', { products });
        } catch (error) {
         console.log(error);
         res.send(error);
@@ -15,7 +25,17 @@ class Controller{
 
     static async showBuyer(req,res){
         try {
-         const products = await Product.findAll();
+            let { search } = req.query
+            let option = {
+            };
+            if (search){
+                option.where = {
+                    name:{
+                        [Op.iLike]: `%${search}%`,
+                    },
+                };
+            }
+         const products = await Product.findAll(option);
          res.render('buyerView', { products });
         } catch (error) {
          console.log(error);
